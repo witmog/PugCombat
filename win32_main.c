df942eb42d400797a8adda28cafe39042138934d
Esben Olsen <esben.w.t.olsen@gmail.com>
2019-12-30 15:46:55 -0500

    Create window class

--- win32_main.c
+++ win32_main.c
@@ -6,4 +6,13 @@ WinMain(HINSTANCE instance,
         LPSTR     cmd_line,
         int       show_cmd)
 {
+	WNDCLASSEX wc = {0};	
+	wc.cbSize = sizeof(WNDCLASSEX); 
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.hInstance = instance;
+	wc.lpszClassName = "PugCombat";
+
+	RegisterClassEx(&wc);
+
 }
